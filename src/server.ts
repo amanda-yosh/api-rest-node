377568df6c481a5d42d8c86af0c0f502f04144ab
Amanda Maria Costa de Almeida <amandamca.aa@gmail.com>
2024-05-15 09:12:22 -0300

    chore: eslint config

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
-const app = fastify();
+const app = fastify()
 
 app.get('/hello', () => {
-    return 'Hello!'
+  return 'Hello!'
 })
 
-app.listen({
+app
+  .listen({
     port: 3333,
-}).then(() => {
+  })
+  .then(() => {
     console.log('Server is running on port 3333')
-})
+  })

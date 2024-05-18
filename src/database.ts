58679deacd2b29124dee12e1193eb46f0768ae42
Amanda Maria Costa de Almeida <amandamca.aa@gmail.com>
2024-05-18 10:14:39 -0300

    feat: install and config zod

--- src/database.ts
+++ src/database.ts
@@ -1,19 +1,15 @@
-import 'dotenv/config'
 import { knex as setupKnex, Knex } from 'knex'
-
-if (!process.env.DATABASE_URL) {
-  throw new Error('DATABASE_URL must be set')
-}
+import { env } from './env'
 
 export const config: Knex.Config = {
   client: 'sqlite',
   connection: {
-    filename: process.env.DATABASE_URL, // relative path to root of project
+    filename: env.DATABASE_URL, // relative path to root of project
   },
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
-    directory: './db/migrations',
+    directory: env.MIGRATIONS_PATH,
   },
 }
 

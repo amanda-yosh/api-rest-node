41a54d24150571917528d40ab1fb2c921a52d742
Amanda Maria Costa de Almeida <amandamca.aa@gmail.com>
2024-05-20 23:56:59 -0300

    fix: deploy on render

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,8 @@ import { env } from './env'
 app
   .listen({
     port: env.PORT,
+    host: 'RENDER' in process.env ? '0.0.0.0' : 'localhost',
   })
   .then(() => {
-    console.log('Server is running on port 3333')
+    console.log('Server is running on port' + env.PORT)
   })
